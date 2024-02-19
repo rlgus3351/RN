133157f29e8924a6c5b6935e70abb537dd9628fd
권기현 <rlgus3351@naver.com>
2024-02-19 17:53:23 +0900

    24/02/19 create MyProfile Component

--- part2-ch1-kakao-friend-list/App.js
+++ part2-ch1-kakao-friend-list/App.js
@@ -1,7 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { Platform, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 import Header from './src/Header';
 import { getStatusBarHeight, getBottomSpace } from 'react-native-iphone-x-helper';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import MyProfile from './src/MyProfile';
+import Margin from './src/Margin';
+import { myProfile } from './src/data';
+
 
 const statusBarHeight = getStatusBarHeight(true);
 const bottomSpace = getBottomSpace();
@@ -10,16 +15,24 @@ const bottomSpace = getBottomSpace();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Header/>
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <Header/>
+        <Margin height={10}/>
+        <MyProfile
+          uri={myProfile.uri}
+          name={myProfile.name}
+          introduction={myProfile.introduction}
+        />
+      </SafeAreaView>
+    </SafeAreaProvider>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop:statusBarHeight+30,  
     backgroundColor: '#fff',
   },
 });
---
+++ part2-ch1-kakao-friend-list/src/Margin.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { View } from "react-native";
+
+export default (props) =>{
+    return(
+        <View style={{height:props.height}}/>
+    );
+}
