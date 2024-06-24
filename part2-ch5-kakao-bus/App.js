0f52933c3dcf05f7c9e13bb53e6dc38111fe0452
권기현 <rlgus3351@naver.com>
2024-06-24 17:11:13 +0900

    24/06/24 ListHearderComponent

--- part2-ch5-kakao-bus/App.js
+++ part2-ch5-kakao-bus/App.js
@@ -1,32 +1,63 @@
-
-import { StyleSheet, Text, View, SectionList, FlatList } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import BusInfo from './src/BusInfo';
-import { COLOR } from './src/color';
-import { busStop, getSections, getBusNumColorByType, getRemainedTimeText, getSeatStatusText } from './src/data';
 import dayjs from 'dayjs';
 import { useEffect, useState } from 'react';
-import { SimpleLineIcons } from "@expo/vector-icons"
+import { SafeAreaView, SectionList, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { SimpleLineIcons } from '@expo/vector-icons';
+
+import BusInfo from './src/BusInfo';
+import { COLOR } from './src/color';
+import { busStop, getBusNumColorByType, getRemainedTimeText, getSeatStatusText, getSections } from './src/data';
+import Margin from './src/Margin';
+import BookmarkButtons from './src/BookmarkButtons';
+
+// import BookmarkButton from './src/BookmarkButton';
+
+const busStopBoomarkSize = 20;
+const busStopBoomarkPadding = 6;
 
 export default function App() {
   const sections = getSections(busStop.buses);
-
   const [now, setNow] = useState(dayjs());
-  const ListHeaderComponent = () => {
-    <SafeAreaView style={{ backgroundColor: COLOR.GRAY_1 }}>
-      <View style={{ flexDirection: 'row' }}>
-        <SimpleLineIcons name='arrow-left' size={20} color={COLOR.WHITE} />
-        <SimpleLineIcons name='home' size={20} color={COLOR.WHITE} />
+
+  const onPressBusStopBookmark = () => {
+    busStop.isBookmarked = !busStop.isBookmarked;
+
+  };
+  const ListHeaderComponent = () => (
+    <SafeAreaView style={{ backgroundColor: COLOR.GRAY_3 }}>
+      {/* 뒤로가기, 홈 아이콘 */}
+      <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+        <TouchableOpacity style={{ padding: 10 }}>
+          <SimpleLineIcons name="arrow-left" size={20} color={COLOR.WHITE} />
+        </TouchableOpacity>
+        <TouchableOpacity style={{ padding: 10 }}>
+          <SimpleLineIcons name="home" size={20} color={COLOR.WHITE} />
+        </TouchableOpacity>
       </View>
-    </SafeAreaView>
-  }
+
+      {/* 정류소 번호, 이름, 방향 */}
+      <View style={{ justifyContent: "center", alignItems: "center" }}>
+        <Margin height={10} />
+        <Text style={{ color: COLOR.WHITE, fontSize: 13 }}>{busStop.id}</Text>
+        <Margin height={4} />
+        <Text style={{ color: COLOR.WHITE, fontSize: 20 }}>{busStop.name}</Text>
+        <Margin height={4} />
+        <Text style={{ color: COLOR.GRAY_1, fontSize: 14 }}>{busStop.directionDescription}</Text>
+        <Margin height={20} />
+      </View>
+      <BookmarkButtons
+        isBookmarked={busStop.isBookmarked}
+        onPress={onPressBusStopBookmark}
+        style={{ borderWidth: 0.3, borderColor: COLOR.GRAY_1 }}
+      />
+    </SafeAreaView >
+  )
   const renderSectionHeader = ({ section: { title } }) => (
     <View style={{
       paddingLeft: 13,
       paddingVertical: 3,
       backgroundColor: COLOR.GRAY_1,
-      borderTopWidth: 1,
-      borderBottomWidth: 1,
+      borderTopWidth: 0.5,
+      borderBottomWidth: 0.5,
       borderTopColor: COLOR.GRAY_2,
       borderBottomColor: COLOR.GRAY_2,
     }}>
@@ -48,25 +79,26 @@ export default function App() {
         ? [null]
         : [firstNextBusInfo, secondNextBusInfo];
 
+    // if (bus.num === 2000) {
+    //   console.log(bus.num, 'newNextBusInfos', newNextBusInfos); // TODO: 확인
+    // }
+
     const processedNextBusInfos = newNextBusInfos.map((info) => {
       if (!info)
         return {
           hasInfo: false,
           remainedTimeText: "도착 정보 없음",
         };
+
       const { arrivalTime, numOfRemainedStops, numOfPassengers } = info;
       const remainedTimeText = getRemainedTimeText(now, arrivalTime);
       const seatStatusText = getSeatStatusText(bus.type, numOfPassengers);
-
-
-
       return {
         hasInfo: true,
         remainedTimeText,
         numOfRemainedStops,
         seatStatusText,
       };
-
     });
     /**
      * End
@@ -81,34 +113,30 @@ export default function App() {
         numColor={numColor}
         processedNextBusInfos={processedNextBusInfos}
       />
-    );
+    )
   };
 
   useEffect(() => {
-    // // 1초마다 현재시간 업데이트
-    // const interval = setInterval(() => {
-    //   const newNow = dayjs();
-    //   setNow(newNow);
-    // }, 1000);
+    const interval = setInterval(() => {
+      const newNow = dayjs();
+      setNow(newNow);
+    }, 1000);
 
-    // return () => {
-    //   clearInterval(interval);
-    // };
+    return () => {
+      clearInterval(interval);
+    };
   }, []);
 
   return (
-    <SafeAreaView style={styles.container}>
-
+    <View style={styles.container}>
       <SectionList
         style={{ flex: 1, width: "100%" }}
         sections={sections}
         ListHeaderComponent={ListHeaderComponent}
         renderSectionHeader={renderSectionHeader}
         renderItem={renderItem}
-      >
-
-      </SectionList>
-    </SafeAreaView>
+      />
+    </View>
   );
 }
 
@@ -118,6 +146,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-
   },
 });
