c177f9dc6c0e1e72a9a9da15ed3d259de14c083c
권기현 <98080954+rlgus3351@users.noreply.github.com>
2024-12-20 16:38:35 +0900

    24/12/20 React atom Component

---
+++ react-component-tutorial/src/components/RemoteImage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {Image as RNImage} from 'react-native';
+
+export class RemoteImage extends React.Component{
+    render(){
+        return(
+            <RNImage
+                source={{uri:this.props.url}}
+                style={[this.props.style, {
+                    width:this.props.width, height:this.props.height}]}
+            
+            />
+        )
+    }
+}
